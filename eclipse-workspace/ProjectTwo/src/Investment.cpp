4bf86899d1b8b8e8e51b5b9c7d42a2216965e241
Jeff Perkinson <jmperkinson@gmail.com>
2020-01-30 20:42:04 -0500

    Create Investment class header and source files.

---
+++ eclipse-workspace/ProjectTwo/src/Investment.cpp
@@ -0,0 +1,10 @@
+/*
+ * Jeff Perkinson
+ * CS-210-J3528
+ * Feb 2020
+ * Project Two Airgead Banking
+ */
+
+
+
+
---
+++ eclipse-workspace/ProjectTwo/src/Investment.h
@@ -0,0 +1,21 @@
+/*
+ * Jeff Perkinson
+ * CS-210-J3528
+ * Feb 2020
+ * Project Two Airgead Banking
+ */
+
+/*
+ * Per the standards, all header files should have #define guards to prevent multiple inclusion.
+ * The format of the symbol name should be <PROJECT>_<PATH>_<FILE>_H_.
+ * To guarantee uniqueness, they should be based on the full path in a project's source tree.
+ */
+
+#ifndef PROJECTTWO_SRC_INVESTMENT_H_
+#define PROJECTTWO_SRC_INVESTMENT_H_
+
+
+
+
+
+#endif /* PROJECTTWO_SRC_INVESTMENT_H_ */
